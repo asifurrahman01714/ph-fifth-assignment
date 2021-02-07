3b254ba7df3a033441529d96703680a3a95695f5
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-02-07 23:01:02 +0600

    All bug and error has been fixed

--- app.js
+++ app.js
@@ -1,69 +1,68 @@
 const submit = document.getElementById('submit');
-        submit.addEventListener('click', function () {
-            const mealName = document.getElementById('mealName');
+submit.addEventListener('click', function () {
+    const mealName = document.getElementById('mealName');
 
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName.value}`)
-                .then(res => res.json())
-                .then(data => {
-                    const mealInput = document.getElementById('mealName').value;
-                    const meal = data.meals;
-                    if (mealInput == "") {
-                        alert("Please Enter your Food Name")
-                    } else if (meal == null) {
-                        alert("Don't Match Any Food Item");
-                    } else {
-                        displayData(data.meals);
-                    }
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const mealInput = document.getElementById('mealName').value;
+            const meal = data.meals;
+            if (mealInput == "") {
+                alert("Please Enter your Food Name")
+            } else if (meal == null) {
+                alert("Don't Match Any Food Item");
+            } else {
+                displayData(data.meals);
+            }
 
-                })
-                .catch(error => console.log(error))
-        });
+        })
+        .catch(error => console.log(error))
+});
 
 
-        //mealData = data.meals
-        const displayData = (mealData) => {
-            const mealDiv = document.getElementById('mealDiv');
-            clearAllYourData('mealDiv');
-            clearAllYourData('mealDetails');
-            //allMeal = data.meals
-            mealData.forEach(allMeal => {
-                const mealDivAppend = document.createElement('div');
-                mealDivAppend.className = 'main-part';
-                
-                const mealInfo = `
+//mealData = data.meals
+const displayData = (mealData) => {
+    const mealDiv = document.getElementById('mealDiv');
+    clearAllYourData('mealDiv');
+    clearAllYourData('mealDetails');
+    //allMeal = data.meals
+    mealData.forEach(allMeal => {
+        const mealDivAppend = document.createElement('div');
+        mealDivAppend.className = 'main-part';
+        const mealInfo = `
                      <img class="img-fluid" src="${allMeal.strMealThumb}" alt="">
                      <h3 class ="meal-name"> ${allMeal.strMeal} </h3>
                      <h5 class ="meal-area"> ${allMeal.strArea} </h5>
                  `;
-                mealDivAppend.innerHTML = mealInfo;
+        mealDivAppend.innerHTML = mealInfo;
 
-                mealDivAppend.addEventListener('click', function () {
-                    singleMealDetails(allMeal.idMeal);
+        mealDivAppend.addEventListener('click', function () {
+            singleMealDetails(allMeal.idMeal);
 
-                });
-                mealDiv.appendChild(mealDivAppend);
-            });
-            document.getElementById('mealName').value = '';
-        };
+        });
+        mealDiv.appendChild(mealDivAppend);
+    });
+    document.getElementById('mealName').value = '';
+};
 
 
-        const singleMealDetails = id => {
-            const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-            fetch(url)
-                .then(res => res.json())
-                .then(data => {
-                    displayMealDetails(data.meals[0]);
-                    console.log(data);
-                    console.log(data.meals[0]);
-                })
-        }
+const singleMealDetails = id => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displayMealDetails(data.meals[0]);
+            //console.log(data);
+            //console.log(data.meals[0]);
+        })
+}
 
-        const displayMealDetails = (allMealDetails) => {
-            const mealDetails = document.getElementById('mealDetails');
-            clearAllYourData('mealDetails');
-            const mealDetailAppend = document.createElement('div');
-                mealDetailAppend.className = 'meal-details';
-                const mealInfo = `
+const displayMealDetails = (allMealDetails) => {
+    const mealDetails = document.getElementById('mealDetails');
+    clearAllYourData('mealDetails');
+    const mealDetailAppend = document.createElement('div');
+    mealDetailAppend.className = 'meal-details';
+    const mealInfo = `
                      <img class="detail-img" src="${allMealDetails.strMealThumb}" alt="">
                      <h3 class ="meal-name"> ${allMealDetails.strMeal} </h3>
                      <h3 class ="meal-name"> Ingredients </h3>
@@ -80,12 +79,12 @@ const submit = document.getElementById('submit');
                     </ul
 
                  `;
-                mealDetailAppend.innerHTML = mealInfo;
-                mealDetails.appendChild(mealDetailAppend);
-           
-        }
+    mealDetailAppend.innerHTML = mealInfo;
+    mealDetails.appendChild(mealDetailAppend);
 
-        const clearAllYourData = id => {
-            const mealDetails = document.getElementById(id);
-            mealDetails.innerHTML = "";
-        }
+}
+
+const clearAllYourData = id => {
+    const mealDetails = document.getElementById(id);
+    mealDetails.innerHTML = "";
+}
